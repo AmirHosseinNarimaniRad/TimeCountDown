1be82c9b65a30c47baca9e8ebc45ac935089d2de
AmirHosseinNarimaniRad <Amir.hossein.narimani.rad@gmail.com>
2025-05-12 19:59:50 +0330

    FirstPush

---
+++ Timer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Timer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number in seconds!");
+
+            int Sec = Convert.ToInt32(Console.ReadLine());
+
+            while (Sec >= 0)
+            {
+                Console.WriteLine(Sec);
+                Thread.Sleep(1000); // pause the program for 1 second! 
+                Sec--;
+            }
+
+            Console.WriteLine("Time's Up!");
+
+            Console.ReadLine();
+        }
+    }
+}
