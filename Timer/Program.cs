d5e42612f51efbdb938029d5da4a01624d69012a
AmirHosseinNarimaniRad <Amir.hossein.narimani.rad@gmail.com>
2025-05-12 20:07:45 +0330

    Using Stopwatch
    the most accurate way to count down in seconds

--- Timer/Program.cs
+++ Timer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Timer
@@ -11,12 +12,33 @@ namespace Timer
 
             int Sec = Convert.ToInt32(Console.ReadLine());
 
-            while (Sec >= 0)
+            /*while (Sec >= 0)
             {
                 Console.WriteLine(Sec);
                 Thread.Sleep(1000); // pause the program for 1 second! 
                 Sec--;
+            }*/
+
+            // To ensure more precise second intervals, use Stopwatch instead of relying solely on Thread.Sleep
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            for (int i = Sec; i >=0; i--)
+            {
+                stopwatch.Restart();
+                Console.WriteLine(i);
+
+                while (stopwatch.ElapsedMilliseconds <1000)
+                {
+                    //wait until a second passes 
+
+                    Thread.Sleep(10);//Use Thread.Sleep(10) in a timing loop to reduce CPU usage while waiting
+                }
+                    
+                    
             }
+
+
 
             Console.WriteLine("Time's Up!");
 
